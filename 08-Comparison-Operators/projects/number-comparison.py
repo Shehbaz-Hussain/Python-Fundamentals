d4371b3cc45c6362be36c11a414991e0a13fb381
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-07-10 00:35:57 +0500

    Add salary and number comparison projects

---
+++ 08-Comparison-Operators/projects/number-comparison.py
@@ -0,0 +1,11 @@
+# Number Comparison
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+print("First number is greater:", number1 > number2)
+print("First number is less:", number1 < number2)
+print("Both numbers are equal:", number1 == number2)
+print("Numbers are not equal:", number1 != number2)
+print("First number is greater than or equal to the second:", number1 >= number2)
+print("First number is less than or equal to the second:", number1 <= number2)
---
+++ 08-Comparison-Operators/projects/salary-comparison.py
@@ -0,0 +1,8 @@
+# Salary Comparison
+
+employee1_salary = float(input("Enter the salary of Employee 1: "))
+employee2_salary = float(input("Enter the salary of Employee 2: "))
+
+print("Employee 1 has a higher salary:", employee1_salary > employee2_salary)
+print("Employee 2 has a higher salary:", employee1_salary < employee2_salary)
+print("Both employees have the same salary:", employee1_salary == employee2_salary)
