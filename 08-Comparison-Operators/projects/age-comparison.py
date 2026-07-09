01270d812ba9bc2249f01beb51673807d59aef68
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-07-10 00:35:45 +0500

    Add age and marks comparison projects

---
+++ 08-Comparison-Operators/projects/age-comparison.py
@@ -0,0 +1,8 @@
+# Age Comparison
+
+person1_age = int(input("Enter the age of the first person: "))
+person2_age = int(input("Enter the age of the second person: "))
+
+print("First person is older:", person1_age > person2_age)
+print("Second person is older:", person1_age < person2_age)
+print("Both have the same age:", person1_age == person2_age)
---
+++ 08-Comparison-Operators/projects/marks-comparison.py
@@ -0,0 +1,8 @@
+# Marks Comparison
+
+student1_marks = int(input("Enter marks of Student 1: "))
+student2_marks = int(input("Enter marks of Student 2: "))
+
+print("Student 1 has higher marks:", student1_marks > student2_marks)
+print("Student 2 has higher marks:", student1_marks < student2_marks)
+print("Both students have equal marks:", student1_marks == student2_marks)
