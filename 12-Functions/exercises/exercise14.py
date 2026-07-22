09c1ad1b912df65781d0f0f3b2a023de4483177e
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-07-22 23:20:06 +0500

    Add exercise 14 on variable scope

---
+++ 12-Functions/exercises/exercise14.py
@@ -0,0 +1,36 @@
+"""
+File: 12-Functions/exercises/exercise14.py
+Topic: Variable Scope
+
+Description:
+This exercise helps you practice understanding the difference
+between local and global variables. You will create variables
+with different scopes and observe how they are used inside
+functions.
+"""
+
+# ==========================================================
+# Exercise
+# ==========================================================
+#
+# Create a global variable named course_name and assign it
+# the value:
+#
+# Python Programming Foundation
+#
+# Write a function named display_course() that:
+#
+# • Displays the value of the global variable course_name.
+# • Creates a local variable named lesson with the value:
+#   Functions
+# • Displays the value of the local variable.
+#
+# In the main program:
+#
+# • Call the display_course() function.
+# • Display the value of the global variable course_name.
+#
+# ==========================================================
+
+
+# Write your function below
