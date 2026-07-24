7020cacb3240c454e4193c0e40dc7d61b3adae84
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-07-24 22:41:06 +0500

    Add example 16 demonstrating data structures concept

---
+++ 13-Data-Structures/examples/example16.py
@@ -0,0 +1,32 @@
+"""
+Module 13 - Data Structures
+Example 16: Iterating Over a Set
+
+Purpose:
+Demonstrate how to iterate through the elements of a set using
+a for loop.
+"""
+
+# Create a set of programming languages.
+programming_languages = {
+    "Python",
+    "Java",
+    "C++",
+    "JavaScript"
+}
+
+print("Programming Languages:")
+
+# Iterate through each element in the set.
+for language in programming_languages:
+    print(language)
+
+# Expected Output:
+# Programming Languages:
+# Python
+# Java
+# C++
+# JavaScript
+#
+# Note:
+# The order of elements in a set is not guaranteed.
