539dbd2a512be8d78bbaccdff3f1246179da4cd8
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-08-06 17:10:37 +0500

    feat(data-structures): add Solution 02 for list indexing

---
+++ 13-Data-Structures/exercises/solutions/solution02.py
@@ -0,0 +1,20 @@
+"""
+Module 13 - Data Structures
+Solution 02: Modifying a List
+
+Purpose:
+Provide a complete solution for Exercise 02 by demonstrating how to
+modify an existing element in a list using its index.
+"""
+
+# Create a list of colors.
+colors = ["Red", "Green", "Blue"]
+
+# Replace "Green" with "Yellow".
+colors[1] = "Yellow"
+
+# Print the updated list.
+print(colors)
+
+# Expected Output:
+# ['Red', 'Yellow', 'Blue']
