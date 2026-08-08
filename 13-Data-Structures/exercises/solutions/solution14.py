91c52349a9e4a90ada026fbe94dbd38dab6a9c68
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-08-08 22:03:59 +0500

    feat(data-structures): add Solution 14 working with a list of dictionaries

---
+++ 13-Data-Structures/exercises/solutions/solution14.py
@@ -0,0 +1,25 @@
+"""
+Module 13 - Data Structures
+Solution 14: Working with a List of Dictionaries
+
+Solution:
+"""
+
+# Create the list of dictionaries.
+students = [
+    {"name": "Ali", "grade": "A"},
+    {"name": "Sara", "grade": "B"},
+    {"name": "Ahmed", "grade": "A"}
+]
+
+# Print the complete list.
+print(students)
+
+# Print the first student's name.
+print(students[0]["name"])
+
+# Print the second student's grade.
+print(students[1]["grade"])
+
+# Print the third student's name.
+print(students[2]["name"])
