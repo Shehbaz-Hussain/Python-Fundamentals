d0089b7e432f018d21f386663efcd14f1ac6be23
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-08-08 22:03:59 +0500

    feat(data-structures): add Solution 15 applying built-in functions to a list

---
+++ 13-Data-Structures/exercises/solutions/solution15.py
@@ -0,0 +1,24 @@
+"""
+Module 13 - Data Structures
+Solution 15: Using Built-in Functions with a List
+
+Solution:
+"""
+
+# Create a list named scores.
+scores = [45, 78, 92, 64, 81]
+
+# Print the complete list.
+print(scores)
+
+# Print the number of elements.
+print(len(scores))
+
+# Print the smallest value.
+print(min(scores))
+
+# Print the largest value.
+print(max(scores))
+
+# Print the sum of all values.
+print(sum(scores))
