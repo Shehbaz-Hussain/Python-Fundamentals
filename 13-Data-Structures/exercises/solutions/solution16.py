c2ae5cb71cc804476fd9003c4f551ad9b9442a5f
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-08-10 23:09:54 +0500

    feat(data-structures): add solution 16 for list iteration

---
+++ 13-Data-Structures/exercises/solutions/solution16.py
@@ -0,0 +1,16 @@
+"""
+Module 13 - Data Structures
+Solution 16: Iterating Over a List
+
+Solution:
+"""
+
+# Create a list named languages.
+languages = ["Python", "Java", "C++", "JavaScript"]
+
+# Print the complete list.
+print(languages)
+
+# Use a for loop to print each language.
+for language in languages:
+    print(language)
