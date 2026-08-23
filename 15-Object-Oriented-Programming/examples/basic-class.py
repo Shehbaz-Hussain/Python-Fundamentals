707cac5b54fa300301017347006b99bc0ad3f8db
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-08-23 22:11:21 +0500

    examples(oop): add basic class example

---
+++ 15-Object-Oriented-Programming/examples/basic-class.py
@@ -0,0 +1,19 @@
+"""
+Topic: Basic Classes
+
+Description:
+Demonstrates how to define a simple Python class and create an object
+from that class.
+"""
+
+
+class Student:
+    """Represent a student."""
+
+    pass
+
+
+student = Student()
+
+print(type(student))
+print("Student object created successfully.")
