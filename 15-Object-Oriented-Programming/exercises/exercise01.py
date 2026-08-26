53144017333427d6962fe877b21063c63ed93809
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-08-27 03:13:48 +0500

    Add OOP exercise 01

---
+++ 15-Object-Oriented-Programming/exercises/exercise01.py
@@ -0,0 +1,17 @@
+"""
+Exercise 01: Create a Basic Class
+
+Problem:
+Create a Python class named Student that represents a student.
+
+Requirements:
+1. Define a class named Student.
+2. Add an instance attribute named name.
+3. Create one Student object with the name "Ali".
+4. Display the student's name.
+
+Expected Behavior:
+The program should create a Student object and display:
+
+Name: Ali
+"""
