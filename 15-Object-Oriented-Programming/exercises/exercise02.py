4102249f024271544cbff8122fb1dfb323abbdae
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-08-27 03:13:48 +0500

    Add OOP exercise 02

---
+++ 15-Object-Oriented-Programming/exercises/exercise02.py
@@ -0,0 +1,23 @@
+"""
+Exercise 02: Create Multiple Objects
+
+Problem:
+Create a Python class named Student that represents a student.
+Create multiple Student objects with different names.
+
+Requirements:
+1. Define a class named Student.
+2. Add an instance attribute named name.
+3. Create three Student objects with the names:
+   - Ali
+   - Ayesha
+   - Hamza
+4. Display the name of each student.
+
+Expected Behavior:
+The program should display:
+
+Name: Ali
+Name: Ayesha
+Name: Hamza
+"""
